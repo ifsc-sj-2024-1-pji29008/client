fcecdef74d079c5b96d220ac9b6f5aee6f28b95a
Yago Castro Rosa <yago.c@aluno.ifsc.edu.br>
2024-07-30 15:18:16 -0300

    feat(streamlit): adicionando a aplicação do streamlit.

--- app/config.py
+++
@@ -1,3 +0,0 @@
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///test_jig.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
---
+++ app/home.py
@@ -0,0 +1,43 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+from screens.short_circuit_page import short_circuit_page
+from screens.pin_page import pin_page
+from screens.temperature_page import temperature_page
+
+# Configuração de metadados da página
+st.set_page_config(
+    page_title="Jiga de testes",
+    layout="wide",
+)
+
+# Adiciona o logo da SensorWeb na sidebar
+# with st.sidebar:
+#     st.image("app/assets/logo.png")
+
+# Cria navegação entre as páginas
+page = option_menu(
+    None,
+    ["Página inicial", "Detecção de Curto", "Posição dos Pinos", "Teste de Temperatura"],
+    icons=["🏠", "slash-circle", "crosshair", "thermometer-half"],
+    default_index=0,
+    orientation="horizontal",
+    key="om_solar",
+    styles={
+        "nav-link-selected": {"background-color": "#00aaff"},
+    },
+)
+
+match page:
+    case "Página inicial":
+        st.title("Página inicial")
+        st.write("Selecione uma das opções no menu acima para visualizar os resultados dos testes.")
+
+    case "Detecção de Curto":
+        short_circuit_page()
+
+    case "Posição dos Pinos":
+        pin_page()
+
+    case "Teste de Temperatura":
+        temperature_page()
